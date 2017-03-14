4b00d47ab010263381ff855d945f0a9e971a1cac
t_halds <t_halds@SFOPC0G4J94.ads.autodesk.com>
2017-03-13 21:29:35 -0700

    Add AWS Lambda Project

---
+++ MyAWSLambda/src/example/Hello.java
@@ -0,0 +1,17 @@
+package example;
+
+import com.amazonaws.services.lambda.runtime.Context; 
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+
+public class Hello {
+    public String myHandler(int myCount, Context context) {
+        LambdaLogger logger = context.getLogger();
+        logger.log("received : " + myCount);
+        
+        //print on the console
+        System.out.println("Hello. Welcome. Thanks for uploading..");
+        
+        
+        return String.valueOf(myCount);
+    }
+}
